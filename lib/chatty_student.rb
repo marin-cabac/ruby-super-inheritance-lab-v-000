0d8210c02b882c0f618c2e3117473b5ca51e6890
Marin Cabac <marin.cabac@gmail.com>
2018-03-13 08:58:10 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,7 +6,7 @@ class ChattyStudent < Student
          What, you don't want any spoilers? Okay well let me just tell you who died..."
        end
        def raise_hand
-         10.times(super)
+         10.times{super}
        end
 
 end
